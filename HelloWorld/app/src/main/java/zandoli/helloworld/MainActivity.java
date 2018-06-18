c867513542432276e8fe72d880ebb341ec13550a
zandoli <vaughnaimable@gmail.com>
2018-06-18 11:45:24 -0400

    Initial commit

---
+++ HelloWorld/app/src/main/java/zandoli/helloworld/MainActivity.java
@@ -0,0 +1,15 @@
+package zandoli.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Look this is the layout file I am linking
+        //setContentView(R.layout.activity_main);
+    }
+}
